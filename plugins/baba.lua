361164c2c4205b2a12237182ad7555863e160b78
alimessibot <aliamiralizadebot@gmail.com>
2016-04-28 11:49:09 +0430

    Create baba.lua

---
+++ plugins/baba.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "با بابام چیکار داری دیوث😡"
+  end
+return {
+  description = "My Dad", 
+  usage = "admin name: mehran,omid",
+  patterns = {
+    "^علی",
+
+"^[Aa][Ll][Ii]",
+"^@Xx_fire_boy_eblis_xX",
+
+  },
+  run = run
+}
+end
+
+created by omid_eblis
+chanal:@eblis_unity
+کس ننت اگ بدون ذکر منبع کپی کنی
