abcb25228f8d53c621878a3e96eb68b75d8ff444
alimessibot <aliamiralizadebot@gmail.com>
2016-04-28 10:47:44 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,7 +263,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {184654763,172442430,212782594},
+    sudo_users = {92705454,196314022},
     admin_users = {},
     disabled_channels = {}
   }
