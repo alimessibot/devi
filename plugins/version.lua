3a5beaf934fd747e0bcf1d250c878caa721d9986
alimessibot <aliamiralizadebot@gmail.com>
2016-04-29 10:04:42 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -9,7 +9,7 @@
 do
 
 function run(msg, matches)
-  return '☠Telekiller robot V1☠\n                                                                        💥created by Eblis team💥\n\n 🏅 sudo users 🏅                                                                                                                                                             @kos_khahare_zendegi                                        @Xx_fire_boy_eblis_xX                                                                         ️\n ➡️ our chanal: comming soon⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
+  return '☠Telekiller robot V1☠\n                                                                        💥created by Eblis team💥\n\n 🏅 sudo users 🏅                                                                                                                                                             @kos_khahare_zendegi                                        @Xx_fire_boy_eblis_xX                                                                         ️\n ➡️ our chanal: :/⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
   
 end
 
